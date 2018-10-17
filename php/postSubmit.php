f406e14a6fafd6da69540f0d8083c84d93a71146
seanblee <bawapi@emailsy.info>
2018-10-17 19:04:00 -0400

    fix

--- php/postSubmit.php
+++ php/postSubmit.php
@@ -7,11 +7,7 @@ $link = $_POST["link"];
 $caption = $_POST["caption"];
 $username = $_SESSION['username'];
 $accountNum = $dbh->query("SELECT accountNumber FROM Users WHERE username = '$username'");
-echo $caption
-echo $link
-echo $accountNum;
-echo "Submit";
-/*
+
 try {
 	$sql = "INSERT INTO Posts (imgLink, postText, userID) VALUES ('$link', '$caption', '$accountNum')";
 	$dbh->exec($sql);
@@ -19,5 +15,4 @@ try {
 }
 catch(PDOException $e) {
 	echo "<h3>FAIL</h3>";
-}*/
-$conn = null;
+}
