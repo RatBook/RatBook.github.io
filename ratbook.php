4465e966a336897f8f9b32cb1708b7a0e9abfafa
seanblee <bawapi@emailsy.info>
2018-10-17 17:59:10 -0400

    posts and submit

--- ratbook.php
+++ ratbook.php
@@ -16,5 +16,23 @@
               echo $_SESSION['username'];
             ?>
 		</h1>
+
+		<h2>Submit Post</h2>
+		<form name="postSubmit" action="php/postSubmit.php" method="POST">
+			<input name="link" type="text" placeholder="Link" required>
+			<input name="caption" type="text" placeholder="Caption" required />
+			<button type="submit" class="btn btn-primary">Submit</button>
+		</form> 
+
+		<h2>Posts</h2>
+		<?php
+				include 'dbconnect.php';
+				$rows = $dbh->query("SELECT imgLink, postText FROM Posts");
+				foreach($rows as $row) {
+				echo "<tr>".$row[0]."</td>";
+				echo "<tr>".$row[1]."</td>";
+		    	}
+		    	$dbh = null;
+		?>
 	</body>
 </html>
