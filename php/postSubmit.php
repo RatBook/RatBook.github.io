8cb55380d0a9dff6f9caa1786b942c6085b4f797
seanblee <bawapi@emailsy.info>
2018-10-17 19:03:06 -0400

    fix

--- php/postSubmit.php
+++ php/postSubmit.php
@@ -1,17 +1,17 @@
 <?php
 include_once 'dbconnect.php';
 
-$link = $_POST["link"];
-$caption = $_POST["caption"];
-
 session_start();
 
+$link = $_POST["link"];
+$caption = $_POST["caption"];
 $username = $_SESSION['username'];
 $accountNum = $dbh->query("SELECT accountNumber FROM Users WHERE username = '$username'");
+echo $caption
+echo $link
 echo $accountNum;
 echo "Submit";
-
-
+/*
 try {
 	$sql = "INSERT INTO Posts (imgLink, postText, userID) VALUES ('$link', '$caption', '$accountNum')";
 	$dbh->exec($sql);
@@ -19,5 +19,5 @@ try {
 }
 catch(PDOException $e) {
 	echo "<h3>FAIL</h3>";
-}
+}*/
 $conn = null;
