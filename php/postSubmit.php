a7441b212c124f891b64c4d2cbb03026f3e3d110
seanblee <bawapi@emailsy.info>
2018-10-17 18:49:30 -0400

    test

--- php/postSubmit.php
+++ php/postSubmit.php
@@ -8,10 +8,11 @@ try {
 	session_start();
 	$_SESSION['username'] = $username;
 	die();
+	$accountNum = $dbh->query("SELECT accountNumber FROM Users WHERE username = '$username'");
 	$sql = "INSERT INTO Posts (imgLink, postText, userID)
-	VALUES ('$link', '$caption', '$username')";
+	VALUES ('$link', '$caption', '$accountNum')";
 	$dbh->exec($sql);
-
+	echo "<h3>DONE</h3>";
 }
 catch(PDOException $e) {
 	echo "<h3>FAIL</h3>";
