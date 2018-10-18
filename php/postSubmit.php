3ee021e42ae41f81e9ff0c7581a5bdf3eb371089
Sean Lee <bawapi@emailsy.info>
2018-10-18 16:58:47 -0400

    commment thread

--- php/postSubmit.php
+++ php/postSubmit.php
@@ -35,27 +35,27 @@ $fh = fopen($thread, 'w');
 $newPage = "
 <html>
 	<body>
-		<img src = ".$link.">
-		<h1>".$caption."</h1>
+		<img src = '$link'>
+		<h1>'$caption'</h1>
 		<h2>Comments</h2>
-		<form name="commentSubmit" action="commentSubmit.php" method="POST">
-			<input name="comment" type="text" placeholder="Comment" required />
-			<button type="submit" class="btn btn-primary">Submit</button>
+		<form name='commentSubmit' action='commentSubmit.php' method='POST'>
+			<input name='comment' type='text' placeholder='Comment' required />
+			<button type='submit' class='btn btn-primary'>Submit</button>
 		</form> 
 		<?php
 			include 'php/dbconnect.php';
-			$postNum =$_GET["post"];
-			$rows = $dbh->query("SELECT comment, userID, timestamp FROM Commments WHERE postID = '$postNum' ORDER BY commentID DESC");
+			$postNum = $_GET['post'];
+			$rows = $dbh->query('SELECT comment, userID, timestamp FROM Commments WHERE postID = '$postNum' ORDER BY commentID DESC');
 			
 			foreach($rows as $row) {
-				$subUser = $dbh->query("
+				$subUser = $dbh->query('
 					SELECT username 
 					FROM Users 
 					WHERE accountNumber = '$row[1]'
-				")->fetchColumn(0);
+				')->fetchColumn(0);
 				
-				echo "<h3>".$row[0]."</a></h3></li>";
-				echo "<h3> Submitted by: ".$subUser." on ".$row[2]."</h3>";
+				echo '<h3>'.$row[0]."</a></h3></li>";
+				echo '<h3> Submitted by: '.$subUser.' on '.$row[2].'</h3>'';
 			}
 		?>
 	</body>
